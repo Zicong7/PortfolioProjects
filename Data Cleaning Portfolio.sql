504c520c7607c936f876213d74a233cf089eddcf
Zicong7 <zicong7@vt.edu>
2022-01-28 07:43:02 -0500

    Update Data Cleaning Portfolio.sql

--- Data Cleaning Portfolio.sql
+++ Data Cleaning Portfolio.sql
@@ -2,7 +2,6 @@
 Cleaning Data in SQL Queries
 */
 
-
 Select *
 From PortfolioProject.dbo.NashvilleHousing
 
@@ -10,23 +9,16 @@ From PortfolioProject.dbo.NashvilleHousing
 
 -- Standardize Date Format
 
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
+Add SaleDateConverted Date;
+
+Update PortfolioProject.dbo.NashvilleHousing
+SET SaleDateConverted = CONVERT(Date,SaleDate)
 
 Select saleDateConverted, CONVERT(Date,SaleDate)
 From PortfolioProject.dbo.NashvilleHousing
 
-
-Update NashvilleHousing
-SET SaleDate = CONVERT(Date,SaleDate)
-
--- If it doesn't Update properly
-
-ALTER TABLE NashvilleHousing
-Add SaleDateConverted Date;
-
-Update NashvilleHousing
-SET SaleDateConverted = CONVERT(Date,SaleDate)
-
-
+-- Add SaleDateConverted and Drop SaleDate later on
  --------------------------------------------------------------------------------------------------------------------------
 
 -- Populate Property Address data
@@ -37,7 +29,7 @@ From PortfolioProject.dbo.NashvilleHousing
 order by ParcelID
 
 
-
+-- self join
 Select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress,b.PropertyAddress)
 From PortfolioProject.dbo.NashvilleHousing a
 JOIN PortfolioProject.dbo.NashvilleHousing b
@@ -68,29 +60,26 @@ From PortfolioProject.dbo.NashvilleHousing
 --order by ParcelID
 
 Select
-SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 ) as Address
-, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress)) as Address
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 ) as Address1
+, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress)) as Address2
 
 --CHARINDEX is the number of position
-
---  Address1 = SUBSTRING_INDEX(PropertyAddress, ' ', 1),
---  Address2 = SUBSTRING_INDEX(PropertyAddress, ' ', -1)
-
 From PortfolioProject.dbo.NashvilleHousing
 
+--  CONCAT() adds two or more strings together
 
 
-ALTER TABLE NashvilleHousing
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
 Add PropertySplitAddress Nvarchar(255);
 
-Update NashvilleHousing
+Update PortfolioProject.dbo.NashvilleHousing
 SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 )
 
 
-ALTER TABLE NashvilleHousing
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
 Add PropertySplitCity Nvarchar(255);
 
-Update NashvilleHousing
+Update PortfolioProject.dbo.NashvilleHousing
 SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress))
 
 
@@ -115,25 +104,25 @@ From PortfolioProject.dbo.NashvilleHousing
 
 
 
-ALTER TABLE NashvilleHousing
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
 Add OwnerSplitAddress Nvarchar(255);
 
-Update NashvilleHousing
+Update PortfolioProject.dbo.NashvilleHousing
 SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
 
 
-ALTER TABLE NashvilleHousing
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
 Add OwnerSplitCity Nvarchar(255);
 
-Update NashvilleHousing
+Update PortfolioProject.dbo.NashvilleHousing
 SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
 
 
 
-ALTER TABLE NashvilleHousing
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
 Add OwnerSplitState Nvarchar(255);
 
-Update NashvilleHousing
+Update PortfolioProject.dbo.NashvilleHousing
 SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
 
 
@@ -166,7 +155,7 @@ Select SoldAsVacant
 From PortfolioProject.dbo.NashvilleHousing
 
 
-Update NashvilleHousing
+Update PortfolioProject.dbo.NashvilleHousing
 SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
 	   When SoldAsVacant = 'N' THEN 'No'
 	   ELSE SoldAsVacant
@@ -182,20 +171,10 @@ SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
 -- Remove Duplicates
 
 WITH RowNumCTE AS(
-Select *,
-	ROW_NUMBER() OVER (
-	PARTITION BY ParcelID,
-				 PropertyAddress,
-				 SalePrice,
-				 SaleDate,
-				 LegalReference
-				 ORDER BY
-					UniqueID
-					) row_num
-
+Select *, ROW_NUMBER() OVER (PARTITION BY ParcelID, PropertyAddress, SalePrice, SaleDate, LegalReference ORDER BY UniqueID) row_num
 From PortfolioProject.dbo.NashvilleHousing
---order by ParcelID
 )
+
 Select *
 From RowNumCTE
 Where row_num > 1
@@ -209,7 +188,7 @@ From PortfolioProject.dbo.NashvilleHousing
 
 
 
----------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Delete Unused Columns
 
@@ -222,66 +201,3 @@ From PortfolioProject.dbo.NashvilleHousing
 ALTER TABLE PortfolioProject.dbo.NashvilleHousing
 DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress, SaleDate
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------
-
---- Importing Data using OPENROWSET and BULK INSERT	
-
---  More advanced and looks cooler, but have to configure server appropriately to do correctly
---  Wanted to provide this in case you wanted to try it
-
-
---sp_configure 'show advanced options', 1;
---RECONFIGURE;
---GO
---sp_configure 'Ad Hoc Distributed Queries', 1;
---RECONFIGURE;
---GO
-
-
---USE PortfolioProject 
-
---GO 
-
---EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'AllowInProcess', 1 
-
---GO 
-
---EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'DynamicParameters', 1 
-
---GO 
-
-
----- Using BULK INSERT
-
---USE PortfolioProject;
---GO
---BULK INSERT nashvilleHousing FROM 'C:\Temp\SQL Server Management Studio\Nashville Housing Data for Data Cleaning Project.csv'
---   WITH (
---      FIELDTERMINATOR = ',',
---      ROWTERMINATOR = '\n'
---);
---GO
-
-
----- Using OPENROWSET
---USE PortfolioProject;
---GO
---SELECT * INTO nashvilleHousing
---FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0',
---    'Excel 12.0; Database=C:\Users\alexf\OneDrive\Documents\SQL Server Management Studio\Nashville Housing Data for Data Cleaning Project.csv', [Sheet1$]);
---GO
